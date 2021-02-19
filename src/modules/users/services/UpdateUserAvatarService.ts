adad241f3269a146bda3468a2c38d4bb9d62483c
Jose Italo <itlcoorporativo@gmail.com>
2021-02-19 07:44:36 -0300

    Adicionando o Provider de Storage e criando o teste de Atualização do Avatar

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,11 +1,8 @@
-import path from 'path';
-import fs from 'fs';
 import { injectable, inject } from 'tsyringe';
-
-import uploadConfig from '@config/upload';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 import User from '../infra/typeorm/entities/User';
 
@@ -20,6 +17,9 @@ class UpdateUserAvatarService {
     constructor (
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('StorageProvider')
+        private storageProvider: IStorageProvider,
     ) {}
 
     public async execute ({ user_id, avatarFilename }: IRequest): Promise<User>{
@@ -30,17 +30,12 @@ class UpdateUserAvatarService {
             throw new AppError('Only authenticated users can change avatar.', 401);
         }
         if(user.avatar){
-            //deletar avatar anterior
-
-            const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-            const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-            if (userAvatarFileExists){
-                await fs.promises.unlink(userAvatarFilePath);
-            }
+            await this.storageProvider.deleteFile(user.avatar);
         }
 
-        user.avatar = avatarFilename;
+        const fileName = await this.storageProvider.savefile(avatarFilename);
+
+        user.avatar = fileName;
 
         await this.usersRepository.save(user);
 
