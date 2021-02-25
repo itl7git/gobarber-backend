3b563aca2956de530f0b8a14eb75a809b4dbb695
Jose Italo <itlcoorporativo@gmail.com>
2021-02-25 10:07:56 -0300

    Adicionando template engine

--- src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -1,11 +1,15 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from "../models/IMailTemplateProvider";
 
 class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
-    public async parse({ template, variables }: IParseMailTemplateDTO): Promise<string> {
-        const parsedTemplate = handlebars.compile(template);
+    public async parse({ file, variables }: IParseMailTemplateDTO): Promise<string> {
+        const templateFileContent = await fs.promises.readFile(file , {
+            encoding: 'utf-8',
+        })
+        const parsedTemplate = handlebars.compile(templateFileContent);
 
         return parsedTemplate(variables);
     }
