27be2f3de86e588114b44f831df1b49d0370cbe8
Jose Italo <itlcoorporativo@gmail.com>
2021-03-09 16:31:32 -0300

    Utilizando o Class Transformer

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
+import { Exclude, Expose } from 'class-transformer';
 
 @Entity('users')
 class User {
@@ -13,6 +14,7 @@ class User {
     email: string;
 
     @Column()
+    @Exclude()
     password: string;
 
     @Column()
@@ -24,6 +26,12 @@ class User {
     @UpdateDateColumn()
     updated_at: Date;
 
+    @Expose({ name: 'avatar_url' })
+    getAvatarUrl(): string | null {
+        return this.avatar
+        ? `${process.env.APP_API_URL}/files/${this.avatar}`
+        : null;
+    }
 }
 
 export default User;
